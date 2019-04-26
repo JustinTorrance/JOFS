99fcffc58dcf204c156a9e99cf031e28706c8790
Justin Torrance <justin.torrance91@gmail.com>
2019-04-26 10:24:34 -0600

    Implement Materiaul UI to style cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import Home from '../Home/Home';
 import MeetDoctor from '../MeetDoctor/MeetDoctor';
+import Patients from '../Patients/Patients'
 
 class App extends Component {
   render() {
@@ -11,13 +12,14 @@ class App extends Component {
           <NavLink to='/' exact className='nav'>Home</NavLink>
           <NavLink to='/meet' className='nav'>Meet Dr. Hicks</NavLink>
           <NavLink to='/procedures' className='nav'>Procedures and Services</NavLink>
-          <NavLink to='/patient' className='nav'>Patient Care</NavLink>
+          <NavLink to='/patients' className='nav'>Patient Care</NavLink>
           <NavLink to='/doctors' className='nav'>Referring Doctors</NavLink>
           <NavLink to='/contact' className='nav'>Contact Us</NavLink>
         </div>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/meet' component={MeetDoctor} />
+          <Route path='/patients' component={Patients} />
         </Switch>
         <p className='copyright'>Jackson Oral and Facial Surgery &copy; 2019</p>
       </div>
