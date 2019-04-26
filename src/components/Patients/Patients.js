b7101ec9ded34b99b09bac2b873ab773422c499f
Justin Torrance <justin.torrance91@gmail.com>
2019-04-26 12:06:53 -0600

    Add new favicon

--- src/components/Patients/Patients.js
+++ src/components/Patients/Patients.js
@@ -1,34 +1,20 @@
-// import React from 'react'
-// import Collapse from '@material-ui/core/Collapse'
-
-
-
   import React from 'react';
-  // import PropTypes from 'prop-types';
+  import PropTypes from 'prop-types';
   import { withStyles } from '@material-ui/core/styles';
   import classnames from 'classnames';
   import Card from '@material-ui/core/Card';
   import CardHeader from '@material-ui/core/CardHeader';
-  import CardMedia from '@material-ui/core/CardMedia';
   import CardContent from '@material-ui/core/CardContent';
   import CardActions from '@material-ui/core/CardActions';
   import Collapse from '@material-ui/core/Collapse';
-  // import Avatar from '@material-ui/core/Avatar';
   import IconButton from '@material-ui/core/IconButton';
   import Typography from '@material-ui/core/Typography';
-  // import red from '@material-ui/core/colors/red';
-  // import FavoriteIcon from '@material-ui/icons/Favorite';
-  // import ShareIcon from '@material-ui/icons/Share';
   import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-  // import MoreVertIcon from '@material-ui/icons/MoreVert';
   
   const styles = theme => ({
     card: {
-      maxWidth: 400,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
+      maxWidth: 500,
+      cursor: 'pointer',
     },
     actions: {
       display: 'flex',
@@ -40,8 +26,12 @@
         duration: theme.transitions.duration.shortest,
       }),
     },
-    expandOpen: {
-      transform: 'rotate(180deg)',
+    title: {
+      color: '#263e76',
+    },
+    span: {
+      color: '#263e76',
+      fontWeight: '800',
     },
   });
   
@@ -56,28 +46,49 @@
       const { classes } = this.props;
   
       return (
-        <Card className={classes.card}>
+        <Card className={classes.card} onClick={this.handleExpandClick} >
           <CardHeader
-            title="Shrimp and Chorizo Paella"
+            classes={{
+              title: classes.title,
+            }}
+            title="Preparing for Intravenous Anesthesia"
           />
-          <CardActions className={classes.actions} disableActionSpacing>
-            <IconButton
-              className={classnames(classes.expand, {
-                [classes.expandOpen]: this.state.expanded,
-              })}
-              onClick={this.handleExpandClick}
-              aria-expanded={this.state.expanded}
-              aria-label="Show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
           <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
             <CardContent>
-              <Typography paragraph>Method:</Typography>
               <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
+                Patient’s who desire to receive IV anesthesia (be asleep) for their surgery must come 
+                with an empty stomach and an escort. This is essential for your safety and welfare. 
+                Please carefully read and follow these directions:No solid foods for 6 hours prior to 
+                your scheduled appointment. Solid foods include milk products of any kind and non-clear 
+                juices like orange or pineapple
+              </Typography>
+              <Typography paragraph>
+                Small amounts of clear liquids may be consumed until 2 (two) hours before your appointment. Examples include water, soda pop, apple, grape, or cranberry juice and black coffee or tea (no creamer).
+              </Typography>
+              <Typography paragraph>
+                Nothing, including water, can be taken during the 2 (two) hours just before your surgery or you will not be able to receive IV anesthesia
+              </Typography>
+              <Typography paragraph>
+                Medicines that you use regularly should be taken at the normally scheduled time with a sip of water, unless you are directed differently by Dr. Hicks
+              </Typography>
+              <Typography paragraph>
+              <Typography className={classes.span} span>Parent / Guardian:</Typography>
+                Patients who are under the age of 18 or are handicapped must have a parent or legal guardian attend their consultation and surgery appointments.
+              </Typography>
+              <Typography paragraph>
+              
+              </Typography>
+              <Typography paragraph>
+              
+              </Typography>
+              <Typography paragraph>
+              
+              </Typography>
+              <Typography paragraph>
+              
+              </Typography>
+              <Typography paragraph>
+              
               </Typography>
             </CardContent>
           </Collapse>
@@ -86,22 +97,8 @@
     }
   }
   
-  // RecipeReviewCard.propTypes = {
-  //   classes: PropTypes.object.isRequired,
-  // };
+  Patients.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
   
   export default withStyles(styles)(Patients);
-  
-
-
-//   return (
-//     <div>
-//       Patients
-//       <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-
-//       </Collapse>
-//     </div>
-//   )
-// }
-
-// export default Patients
