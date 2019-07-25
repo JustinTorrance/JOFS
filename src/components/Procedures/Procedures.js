d9cfd5af9d449f1279a833d3bcaf60be60e39898
Justin Torrance <justin.torrance91@gmail.com>
2019-07-25 13:56:32 -0600

    Add dental implants section to procedures page

--- src/components/Procedures/Procedures.js
+++ src/components/Procedures/Procedures.js
@@ -116,6 +116,91 @@ const Procedures = () => {
           </p>
           <br/>
         </ExpansionPanel>
+
+        <ExpansionPanel className='card' >
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <h3 className='card-header'>Dental Implants</h3>
+          </ExpansionPanelSummary>
+          <br/>
+          <h4 className='sub-header'>What are dental implants? </h4>
+          <p>
+            Implants can be used to replace missing teeth so that the replacements look and feel like natural teeth.  An implant consists of a titanium metal cylinder, or “post”, that is placed in the jawbone to replace the tooth root. The bone grows around the post and locks it in to place and it becomes an anchor, or base, for the crown. The crown, which looks and feels like a real tooth, is placed on top of the post. Whether you have lost teeth to gum disease, tooth decay, a failed root canal or injury, an implant can make a major difference for your quality of life.
+          </p>
+          <br/>
+          <p>
+            Oral and Maxillofacial Surgeons are implant specialists. They are uniquely qualified to safely and permanently secure dental implants. They can reconstruct bone (link to bone grafting page) in areas where more bone is needed to be able to place the implant. They can also modify the gum tissue around implants to make the new teeth look even more natural.
+          </p>
+          <br />
+          <h4 className='sub-header'>Are dental implants the right choice for me? </h4>
+          <p>
+            A patient with missing teeth is confronted with multiple treatment options. These options include dentures, a fixed bridge, and dental implants. Implants are regarded by many as one of the most important advances in dentistry in the last 50 years and have a 95% success rate.
+          </p>
+          <br/>
+          <p>
+            While the initial cost of implants is slightly more than alternatives, their longevity, functionality and durability make them a good investment. They can last a lifetime and are the better investment in the long run for your overall health, appearance, and well being.
+          </p>
+          <br/>
+          <p>
+            Implants have several advantages over <strong>dentures</strong> including:
+          </p>
+          <ul className='patient-care-list'>
+            <li>
+              Implants have more biting force than dentures. 400 lbs. per square inch vs. 40-50 lbs. of dentures. It’s close to the psi of real teeth
+            </li>
+            <li>
+              Implants don’t require adhesives or cleaning equipment like dentures
+            </li>
+            <li>
+              Implants offer a higher quality of life because you can live confidently without worrying about slipping or clicking dentures or having to limit the foods you eat
+            </li>
+            <li>
+              Implants preserve the jawbone, where dentures may result in bone loss. When one or more teeth are missing you can experience bone loss, which can effect denture fit and incur costs of adjustment or replacement of dentures
+            </li>
+            <li>
+              Since implants look and feel natural, you can have better self-assurance and self-image, and you don’t have to think about your teeth between regular dental checkups
+            </li>
+          </ul>
+          <br/>
+          <p>
+            Implants also offer advantages to <strong>fixed bridges</strong> including:
+          </p>
+          <ul className='patient-care-list'>          
+            <li>
+              95% of implants last 20 or more years. Fixed bridges have an 85% success rate for 10 years and 66% for 15 years
+            </li>
+            <li>
+              Implants do not harm the adjacent teeth. Fixed bridges may require the shaping or cutting down of adjacent healthy teeth
+            </li>
+          </ul>
+          <br/>
+          <h4 className='sub-header'>Will the procedure hurt?</h4>
+          <p>
+            The procedure is usually performed under local anesthesia to minimize discomfort. Some postoperative soreness may be involved but it is usually minimal, due to the training and expertise of Oral and Maxillofacial Surgeons.
+          </p>
+          <br />
+          <h4 className='sub-header'>How much will it cost?</h4>
+          <p>
+            While the initial cost of implants is slightly more than alternatives, their longevity, functionality, and durability make them a good investment. When properly cared for, dental implants can last a lifetime. Price varies depending on the number of teeth you will be replacing and the type of implant. To get an accurate estimate, schedule a consultation with your Oral and Maxillofacial Surgeon.
+          </p>
+          <br />
+          <h4 className='sub-header'>Steps for receiving an implant:</h4>
+          <p><strong>1. Decide</strong> - Visit your dentist to discuss options for replacing missing, broken or soon to be extracted teeth.</p>
+          <p><strong>2. Plan</strong> -  You meet with your Oral and Maxillofacial surgeon for an examination, x-rays, and consultation.</p>
+          <p><strong>3. Placement</strong> -  Oral and Maxillofacial Surgeon surgically places posts, or implants, in your jaw.</p>
+          <p><strong>4. Healing</strong> -  Implants need a couple weeks to stabilized and fully fuse to the jawbone.</p>
+          <p><strong>5. Placement of abutment</strong> -  the dentist or prosthedontist places the abutment (a small piece that connects the post to the crown) and makes an impression of the upper and lower jaws.</p>
+          <p><strong>6. Building the crown</strong> -  The lab uses this impression to create a crown (or prosthetic tooth) that will match and perfectly fit with your natural teeth. </p>
+          <p><strong>7. Final crown</strong> -  Dentist secures the crown in the post.</p>
+          <p><strong>8. Follow up</strong> -  You will meet with Dr. Hicks one last time.</p>
+          <br />
+          <h4 className='sub-header'>For more information on dental implants:</h4>
+          <p>
+            <a href='http://myoms.org/procedures/dental-implant-surgery'>The American Association of Oral and Maxillofacial Surgeons</a>
+          </p>
+          <p>
+            <a href='http://www.aaid-implant.org/about-dental-implants/what-are-dental-implants/'>The American Academy of Implant Dentistry</a>
+          </p>
+        </ExpansionPanel>
     </section>
   )
 }
