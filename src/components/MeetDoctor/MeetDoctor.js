607658f2d8100d497306889f52e01066c3797902
Justin Torrance <justin.torrance91@gmail.com>
2019-09-06 14:22:33 -0600

    Begin styling the Meet Dr Hicks page. Float first two images.

--- src/components/MeetDoctor/MeetDoctor.js
+++ src/components/MeetDoctor/MeetDoctor.js
@@ -10,12 +10,12 @@ import climbing from '../../assets/climbing.jpg'
 const MeetDoctor = () => {
 
   return(
-    <main>
+    <main className='meet-main'>
       <h1 className='meet-doctor-name'>Meet Dr. Hicks</h1>
       <section className='about-doctor'>
         <div className='headshot-div'>
           <img src={doctorHeadshot} alt="Doctor Riley Hicks" className='headshot-img'/>
-          <h6 className='headshot-h6'>Dr. Riley Hicks DDS, Board Certified Specialist</h6>
+          {/* <h6 className='headshot-h6'>Dr. Riley Hicks DDS, Board Certified Specialist</h6> */}
         </div>
 
         <article className='education'>
@@ -42,8 +42,9 @@ const MeetDoctor = () => {
         </article>
 
         <article className='memberships'>
+          <img src={drHicks} className='preparation-img' alt="Doctor Hicks preparing to see a patient" width='400px' />
           <h3 className='meet-header'>Dr. Hicks Maintains Professional Memberships With:</h3>
-          <ul>
+          <ul className='about-ul'>
             <li>American Association of Oral and Maxillofacial Surgeons</li>
             <li>American College of Oral and Maxillofacial Surgeons</li>
             <li>Idaho Society of Oral and Maxillofacial Surgeons</li>
@@ -53,32 +54,31 @@ const MeetDoctor = () => {
             <li>Idaho State Dental Association</li>
             <li>Omicron Kappa Upsilon the National Honorary Dental Society</li>
           </ul>
-          <img src={drHicks} alt="Doctor Hicks preparing to see a patient" width='400px' />
         </article>
 
         <article className='experience'>
           <h3 className='meet-header'>Specialization and Experience</h3>
-          <p>
+          <p className='about-p'>
             Dr. Hicks brings many years of clinical and surgical experience to his practice. 
             Patients can feel confident in his ability to perform the most delicate surgical 
             procedures. Idaho Falls Oral and Facial Surgery opened in June 1998. For the past 
             18 years, Dr. Hicks has provided a full range of surgical services to patients from 
             Eastern Idaho, Western Wyoming and even Salt Lake City.
           </p>
-          <p>
+          <p className='about-p'>
             Dr. Hicks, with his highly trained staff, provide in-office surgical services for 
             removal of third molars, extraction of decayed or damaged teeth in preparation for 
             dentures, or restoration of missing teeth using Straumann dental implants. Dr. Hicks 
             also performs corrective jaw surgery and trauma repair. These services can all be completed 
             while the patient is comfortably asleep using IV anesthesia.
           </p>
-          <p>
+          <p className='about-p'>
             He has also developed a particular specialty in the area of oral and facial lesion 
             removal and pathology. He has shared his knowledge with the local dental community 
             by presenting several seminars for area dentists and hygienists to increase the 
             knowledge base and awareness of oral cancers and lesions.
           </p>
-          <p>
+          <p className='about-p'>
             Dr. Hicks stays on the cutting edge of his field by attending continuing education 
             courses and conferences where he invests significant time learning the latest in 
             treatments and techniques. He is credentialed at both Idaho Falls hospitals and takes 
@@ -89,7 +89,7 @@ const MeetDoctor = () => {
         
         <article className='beyond'>
           <h3 className='meet-header' >Beyond The White Coat</h3>
-          <p>
+          <p className='about-p'>
             Dr. Hicks goes the extra mile in more than just his professional pursuits. His 
             latest hobby is ultra distance running.  His longest race was 69 miles taking him just 
             under 22 hours of continuous running and hiking to complete. He also loves the 
