82f2ef03f5acbee999921da650df0adf5eefc696
Justin Torrance <justin.torrance91@gmail.com>
2019-10-10 14:00:27 -0600

    Add temporary media query at 1440px

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,23 +11,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='header-section'>
-          <NavLink to='/' exact className='nav'>Home</NavLink>
-          <NavLink to='/meet' className='nav'>Meet Dr. Hicks</NavLink>
-          <NavLink to='/procedures' className='nav'>Procedures and Services</NavLink>
-          <NavLink to='/patients' className='nav'>Patient Care</NavLink>
-          <NavLink to='/doctors' className='nav'>Referring Doctors</NavLink>
-          <NavLink to='/contact' className='nav'>Contact Us</NavLink>
+        <div className='content-wrapper'>
+          <div className='header-section'>
+            <NavLink to='/' exact className='nav'>Home</NavLink>
+            <NavLink to='/meet' className='nav'>Meet Dr. Hicks</NavLink>
+            <NavLink to='/procedures' className='nav'>Procedures and Services</NavLink>
+            <NavLink to='/patients' className='nav'>Patient Care</NavLink>
+            <NavLink to='/doctors' className='nav'>Referring Doctors</NavLink>
+            <NavLink to='/contact' className='nav'>Contact Us</NavLink>
+          </div>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/meet' component={MeetDoctor} />
+            <Route path='/patients' component={Patients} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/procedures' component={Procedures} />
+            <Route path='/doctors' component={Doctors} />
+          </Switch>
+          <p className='copyright'>Jackson Oral and Facial Surgery &copy; 2019</p>
         </div>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/meet' component={MeetDoctor} />
-          <Route path='/patients' component={Patients} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/procedures' component={Procedures} />
-          <Route path='/doctors' component={Doctors} />
-        </Switch>
-        <p className='copyright'>Jackson Oral and Facial Surgery &copy; 2019</p>
       </div>
     );
   }
