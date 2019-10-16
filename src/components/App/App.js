5eea72b1563dab94c505f52ad314a099544c164c
Justin Torrance <justin.torrance91@gmail.com>
2019-10-15 21:19:36 -0600

    Add MenuIcon and pop-up menu to nav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,13 +6,42 @@ import Patients from '../Patients/Patients'
 import Contact from '../Contact/Contact'
 import Procedures from '../Procedures/Procedures'
 import Doctors from '../Doctors/Doctors'
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
-  render() {
+
+export default function App () {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+    
     return (
       <div className="App">
         <div className='content-wrapper'>
           <div className='header-section'>
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+              <MenuIcon />
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
             <NavLink to='/' exact className='nav'>Home</NavLink>
             <NavLink to='/meet' className='nav'>Meet Dr. Hicks</NavLink>
             <NavLink to='/procedures' className='nav'>Procedures and Services</NavLink>
@@ -33,6 +62,4 @@ class App extends Component {
       </div>
     );
   }
-}
 
-export default App;
